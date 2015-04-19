3ec6ee18d62d43f91b0b7397f257a23ed1219aed
Varun Chitre <varun.chitre15@gmail.com>
2015-04-19 10:28:52 -0400

    build: migrate build script to 64-bit

--- build.sh
+++ build.sh
@@ -15,13 +15,8 @@
  # Please maintain this if you use this script or any part of it
  #
 KERNEL_DIR=$PWD
-ZIMAGE=$KERNEL_DIR/arch/arm/boot/zImage
-MKBOOTIMG=$KERNEL_DIR/tools/mkbootimg
-MINIGZIP=$KERNEL_DIR/tools/minigzip
-MKBOOTFS=$KERNEL_DIR/tools/mkbootfs
+KERN_IMG=$KERNEL_DIR/arch/arm64/boot/Image
 DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
-ROOTFS=$KERNEL_DIR/root.fs
-BOOTIMG=$KERNEL_DIR/boot.img
 BUILD_START=$(date +"%s")
 blue='\033[0;34m'
 cyan='\033[0;36m'
@@ -29,9 +24,9 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 # Modify the following variable if you want to build
-export CROSS_COMPILE="/root/linaro/4.8.3-2013.11.20131205/bin/arm-linux-gnueabihf-"
-export ARCH=arm
-export SUBARCH=arm
+export CROSS_COMPILE="/root/cm12.1/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export ARCH=arm64
+export SUBARCH=arm64
 export KBUILD_BUILD_USER="varun.chitre15"
 export KBUILD_BUILD_HOST="Monster-Machine"
 
@@ -40,58 +35,24 @@ compile_kernel ()
 echo -e "$blue***********************************************"
 echo "          Compiling ThunderZap kernel          "
 echo -e "***********************************************$nocol"
-make cyanogenmod_tomato_defconfig
-make zImage -j32
-make dtbs
-if ! [ -a $ZIMAGE ];
+make cyanogenmod_tomato-64_defconfig
+make Image -j12
+make dtbs -j12
+$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm64/boot/dts/
+if ! [ -a $KERN_IMG ];
 then
 echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
 exit 1
 fi
 }
 
-compile_bootimg ()
-{
-echo -e "$yellow*************************************************"
-echo "             Creating boot image for $2"
-echo -e "*************************************************$nocol"
-# $MKBOOTFS $1/ > $KERNEL_DIR/ramdisk.cpio
-# cat $KERNEL_DIR/ramdisk.cpio | $MINIGZIP > $KERNEL_DIR/root.fs
-$DTBTOOL -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
-$MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs --cmdline "console=ttyHSL0,115200,n8 boot_cpus=0,4,5,6,7 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci sched_enable_hmp=1"  --base 0x80000000 --pagesize 2048 --dt arch/arm/boot/dt.img -o $KERNEL_DIR/boot.img
-if ! [ -a $ROOTFS ];
-then
-echo -e "$red Ramdisk creation failed $nocol"
-exit 1
-fi
-if ! [ -a $BOOTIMG ];
-then
-echo -e "$red Boot image creation failed $nocol"
-exit 1
-fi
-finally_done
-}
-
-finally_done ()
-{
-echo -e "$cyan BOOT Image installed on: $BOOTIMG$nocol"
-}
-
 case $1 in
-cm11)
-compile_kernel
-compile_bootimg ramdisk-cm11 CM11
-;;
-image-only-cm11)
-compile_bootimg ramdisk Stock
-;;
 clean)
-make ARCH=arm -j8 clean mrproper
-rm -rf $KERNEL_DIR/ramdisk.cpio $KERNEL_DIR/boot.img $KERNEL_DIR/arch/arm/boot/dt.img
+make ARCH=arm64 -j8 clean mrproper
+rm -rf $KERNEL_DIR/arch/arm/boot/dt.img
 ;;
 *)
-echo -e "Add valid option\nValid options are:\n./build.sh (cm11|clean)"
-exit 1
+compile_kernel
 ;;
 esac
 BUILD_END=$(date +"%s")
